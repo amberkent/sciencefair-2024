a84038bb82d784de955a7292622b0dbde3c63a2a
amberkent <122503253+amberkent@users.noreply.github.com>
2024-01-07 15:20:06 -0700

    Update Draft1.py
    Fixing my errors in line 12

--- Draft1.py
+++ Draft1.py
@@ -1,10 +1,18 @@
 import RPi.GPIO as GPIO
 import time
-
-GPIO.setmode(GPIO.BCM)
+import Adafruit_ADS1x15
 PWMpin=0
+Pot_Pin=3
+GAIN=1
+GPIO.setmode(GPIO.BCM)
 GPIO.setup(PWMpin,GPIO.OUT)
 
 p = GPIO.PWM (PWMpin,50) #Frequenzy 50 = 50 hertz
 
-            
+value = adc.read_adc_difference(Pot_Pin, gain=GAIN)
+# the dutycycle for RPi.GPIO is 0-100:
+# See: https://raspberrypi.stackexchange.com/questions/114413/what-values-for-pwm-to-set-intensity-from-0-to-255#:~:text=0-,RPi.,and%2050%20is%20half%20power.
+p.ChangeDutyCycle(dc) 
+
+# 0.05 = 0.05 seconds
+ sleep.time(0.05)
