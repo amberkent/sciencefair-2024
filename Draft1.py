c86a669271322d541552ae88392ce208d124024f
amberkent <122503253+amberkent@users.noreply.github.com>
2024-01-08 07:43:19 -0700

    Update Draft1.py
    adding while true statement

--- Draft1.py
+++ Draft1.py
@@ -5,14 +5,16 @@ PWMpin=0
 Pot_Pin=3
 GAIN=1/327.67
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(PWMpin,GPIO.OUT)
-p = GPIO.PWM (PWMpin,50) #Frequenzy 50 = 50 hertz
+while true:
+ 
+ GPIO.setmode(GPIO.BCM)
+ GPIO.setup(PWMpin,GPIO.OUT)
+ p = GPIO.PWM (PWMpin,50) #Frequenzy 50 = 50 hertz
 
-value = adc.read_adc_difference(Pot_Pin, gain=GAIN)
-# the dutycycle for RPi.GPIO is 0-100:
-# See: https://raspberrypi.stackexchange.com/questions/114413/what-values-for-pwm-to-set-intensity-from-0-to-255#:~:text=0-,RPi.,and%2050%20is%20half%20power.
-p.ChangeDutyCycle(dc) 
+ value = adc.read_adc_difference(Pot_Pin, gain=GAIN)
+ # the dutycycle for RPi.GPIO is 0-100:
+ # See: https://raspberrypi.stackexchange.com/questions/114413/what-values-for-pwm-to-set-intensity-from-0-to-255#:~:text=0-,RPi.,and%2050%20is%20half%20power.
+ p.ChangeDutyCycle(dc) 
 
-# 0.05 = 0.05 seconds
+ # 0.05 = 0.05 seconds
  sleep.time(0.05)
