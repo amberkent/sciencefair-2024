ca0d74295865ea15e765997ff0276123919aa80d
amberkent <122503253+amberkent@users.noreply.github.com>
2024-02-01 19:52:58 -0700

    Update Draft1.py
    Fixing my mistake when I was multiplying.

--- Draft1.py
+++ Draft1.py
@@ -12,10 +12,11 @@ p = GPIO.PWM (PWMpin,50) #Frequenzy 50 = 50 hertz
 
 while true:
 
-    inputValue = GPIO.input(Pot_Pin*GAIN)
+    inputValue = GPIO.input(Pot_Pin)
     #value = adc.read_adc_difference(Pot_Pin, gain=GAIN)
     # the dutycycle for RPi.GPIO is 0-100:
     # See: https://raspberrypi.stackexchange.com/questions/114413/what-values-for-pwm-to-set-intensity-from-0-to-255#:~:text=0-,RPi.,and%2050%20is%20half%20power.
+    pot_pin_value=inputValue*GAIN
     p.ChangeDutyCycle(dc) 
 
  # 0.05 = 0.05 seconds
