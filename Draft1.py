f6ee064fa06346b329129ffce0a0bcf96fd28470
amberkent <122503253+amberkent@users.noreply.github.com>
2024-02-15 06:58:44 -0700

    Update Draft1.py

--- Draft1.py
+++ Draft1.py
@@ -12,13 +12,13 @@ GPIO.setup(PWMpin,GPIO.OUT)
 p = GPIO.PWM (PWMpin,50) #Frequenzy 50 = 50 hertz
 
 while True:
-
-    inputValue = GPIO.input(Pot_Pin)
-    #value = adc.read_adc_difference(Pot_Pin, gain=GAIN)
-    # the dutycycle for RPi.GPIO is 0-100:
-    # See: https://raspberrypi.stackexchange.com/questions/114413/what-values-for-pwm-to-set-intensity-from-0-to-255#:~:text=0-,RPi.,and%2050%20is%20half%20power.
-    pot_pin_value=inputValue*GAIN
-    p.ChangeDutyCycle(dc) 
+    For dc in range (0,101):
+        inputValue = GPIO.input(Pot_Pin)
+        #value = adc.read_adc_difference(Pot_Pin, gain=GAIN)
+        # the dutycycle for RPi.GPIO is 0-100:
+        # See: https://raspberrypi.stackexchange.com/questions/114413/what-values-for-pwm-to-set-intensity-from-0-to-255#:~:text=0-,RPi.,and%2050%20is%20half%20power.
+        pot_pin_value=inputValue*GAIN
+        p.ChangeDutyCycle(dc) 
 
 # 0.05 = 0.05 seconds
 time.sleep(0.05)
