78652524e8db408b9cc6e2b4925b1e634a70f974
Amber Kent <laurelamberkent@gmail.com>
2024-02-23 23:29:46 -0700

    PWM working

--- Draft1.py
+++ Draft1.py
@@ -5,23 +5,22 @@ import adafruit_tsl2591
 import board
 import busio
 from adafruit_ads1x15.analog_in import AnalogIn
+
 PWMpin=19
-#Pot_Pin=7
 GAIN=100/24300
-#GAIN=1
 
 GPIO.setmode(GPIO.BCM)
-#GPIO.setup(Pot_Pin, GPIO.IN)
 GPIO.setup(PWMpin,GPIO.OUT)
-p = GPIO.PWM (PWMpin,100) #Frequenzy 50 = 50 hertz
-i2c = board.I2C()  
+p = GPIO.PWM(PWMpin,0.25) #Frequenzy 50 = 50 hertz
+p.start(0)
+
+i2c = board.I2C()
 i2c = busio.I2C(board.SCL, board.SDA)
 ads = ADS.ADS1115(i2c)
 chan_potentiometer = AnalogIn(ads, ADS.P0)
 
 while True:
     value = chan_potentiometer.value
-    #value = adc.read_adc_difference(Pot_Pin, gain=GAIN)
     # the dutycycle for RPi.GPIO is 0-100:
     # See: https://raspberrypi.stackexchange.com/questions/114413/what-values-for-pwm-to-set-intensity-from-0-to-255#:~:text=0-,RPi.,and%2050%20is%20half%20power.
     dc= min(max(value*GAIN, 0), 100)
